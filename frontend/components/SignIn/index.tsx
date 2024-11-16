e9fbb19e1057dd6c09cd871577e68d51aefa31c6
Xavier D'Mello <xaviermdmello@gmail.com>
2024-11-17 04:50:23 +0700

    noice

--- frontend/components/SignIn/index.tsx
+++ frontend/components/SignIn/index.tsx
@@ -1,118 +1,145 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { Button } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react";
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 import { useAccount } from "wagmi";
-import {Box} from "@chakra-ui/react";
-import mapboxgl from 'mapbox-gl'
-import { useReadContract } from 'wagmi'
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { Box } from "@chakra-ui/react";
+import mapboxgl from "mapbox-gl";
+import { useReadContract } from "wagmi";
+import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useRef } from "react";
-import abi from '../../public/abi'
+import abi from "../../public/abi";
 
 export const SignIn = () => {
-const { address, isConnected, chain } = useAccount();
-const { data: session } = useSession();
-const mapRef = useRef<mapboxgl.Map | null>(null);
-const mapContainerRef = useRef<HTMLDivElement | null>(null);
-const livelyAddress = process.env.NEXT_PUBLIC_LIVELY as `0x${string}`;
+  const { address, isConnected, chain } = useAccount();
+  const { data: session } = useSession();
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const livelyAddress = process.env.NEXT_PUBLIC_LIVELY as `0x${string}`;
 
-const {data: data} = useReadContract({
-  address: livelyAddress,
-  abi: abi,
-  functionName: 'getAllEvents',
-})
-
-useEffect(() => {
-  console.log(data)
-  if (mapContainerRef.current) {
-    mapboxgl.accessToken = 'pk.eyJ1IjoidGFjb2NhdDQ2NDIiLCJhIjoiY20za280d2Z4MGd1azJrczcxMmFuajBueCJ9.LHliYmwenOrUXMiEtWLGxQ';
-
-    // Attempt to get the user's current location
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-
-        // Initialize the map with the user's location as the center
-        mapRef.current = new mapboxgl.Map({
-          container: mapContainerRef.current as HTMLElement,
-          style: 'mapbox://styles/mapbox/streets-v12', // style URL
-          center: [longitude, latitude], // user's location
-          zoom: 10.12 // default zoom
-        });
-
-        // Add geolocate control to the map.
-        mapRef.current.addControl(
-          new mapboxgl.GeolocateControl({
-            positionOptions: {
-              enableHighAccuracy: true
-            },
-            trackUserLocation: true,
-            showUserHeading: true
-          })
-        );
-      },
-      (error) => {
-        console.error("Error getting user's location:", error);
-
-        // Fallback to a default location if user location is not available
-        mapRef.current = new mapboxgl.Map({
-          container: mapContainerRef.current as HTMLElement,
-          style: 'mapbox://styles/mapbox/streets-v12', // style URL
-          center: [-74.0242, 40.6941], // default center
-          zoom: 10.12 // default zoom
-        });
-
-        // Add geolocate control to the map.
-        mapRef.current.addControl(
-          new mapboxgl.GeolocateControl({
-            positionOptions: {
-              enableHighAccuracy: true
-            },
-            trackUserLocation: true,
-            showUserHeading: true
-          })
-        );
-      }
-    );
-
-    return () => {
-      mapRef.current?.remove();
-    };
-  }
-}, []);
-
-return (
-  <>  
-    <Box display="flex" justifyContent="center" alignItems="center" width="100%">
-   {session && <Button colorScheme="red.500" variant="surface" padding="5px" onClick={() => signIn()}>Sign in With Worldcoin</Button>}
-   </Box>
-   {!session && <DynamicWidget />}
-   {!session && <div id='map-container' ref={mapContainerRef}/>}
-
-
- 
-
-  </>
-)
+  const { data: data } = useReadContract({
+    address: livelyAddress,
+    abi: abi,
+    functionName: "getAllEvents",
+  });
   
+  useEffect(() => {
+    if (mapContainerRef.current) {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoidGFjb2NhdDQ2NDIiLCJhIjoiY20za280d2Z4MGd1azJrczcxMmFuajBueCJ9.LHliYmwenOrUXMiEtWLGxQ";
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+
+          mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current as HTMLElement,
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: [longitude, latitude],
+            zoom: 10.12,
+          });
+
+          mapRef.current.addControl(
+            new mapboxgl.GeolocateControl({
+              positionOptions: {
+                enableHighAccuracy: true,
+              },
+              trackUserLocation: true,
+              showUserHeading: true,
+            })
+          );
+
+          if (data) {
+            data.forEach((event) => {
+              const marker = new mapboxgl.Marker()
+                .setLngLat([parseFloat(event.long), parseFloat(event.lat)])
+                .setPopup(
+                  new mapboxgl.Popup({ offset: 25 }).setHTML(
+                    `<h3>${event.name}</h3><a href="${event.lumaLink}" target="_blank">View Event</a>`
+                  )
+                )
+                .addTo(mapRef.current);
+            });
+          }
+        },
+        (error) => {
+          console.error("Error getting user's location:", error);
+
+          mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current as HTMLElement,
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: [-74.0242, 40.6941],
+            zoom: 10.12,
+          });
+
+          mapRef.current.addControl(
+            new mapboxgl.GeolocateControl({
+              positionOptions: {
+                enableHighAccuracy: true,
+              },
+              trackUserLocation: true,
+              showUserHeading: true,
+            })
+          );
+
+          if (data) {
+            data.forEach((event) => {
+              const marker = new mapboxgl.Marker()
+                .setLngLat([parseFloat(event.long), parseFloat(event.lat)])
+                .setPopup(
+                  new mapboxgl.Popup({ offset: 25 }).setHTML(
+                    `<h3>${event.name}</h3><a href="${event.lumaLink}" target="_blank">View Event</a>`
+                  )
+                )
+                .addTo(mapRef.current);
+            });
+          }
+        }
+      );
+
+      return () => {
+        mapRef.current?.remove();
+      };
+    }
+  }, [data]);
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        width="100%"
+      >
+        {session && (
+          <Button
+            colorScheme="red.500"
+            variant="surface"
+            padding="5px"
+            onClick={() => signIn()}
+          >
+            Sign in With Worldcoin
+          </Button>
+        )}
+      </Box>
+      {!session && <DynamicWidget />}
+      {!session && <div id="map-container" ref={mapContainerRef} />}
+    </>
+  );
 };
 
 function SignInThingy() {
   return (
-    <Box 
-      width="100%" 
-      height="100%" 
-      display="flex" 
-      justifyContent="center" 
+    <Box
+      width="100%"
+      height="100%"
+      display="flex"
+      justifyContent="center"
       alignItems="center"
       margin="2rem"
     >
-      <Box>
-
-        
-      </Box>
+      <Box></Box>
     </Box>
-  )
+  );
 }
