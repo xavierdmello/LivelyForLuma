65caaee7f323f03ad8381b943bc3b4889f2961bb
Xavier D'Mello <xaviermdmello@gmail.com>
2024-11-16 19:09:23 +0700

    woo

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 
   providers: [
