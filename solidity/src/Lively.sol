4735abc715f07358bfd6cc0a3b7228ca51a53c8e
Xavier D'Mello <xaviermdmello@gmail.com>
2024-11-16 23:00:47 +0700

    finish contract

---
+++ solidity/src/Lively.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Lively {
+    Event[] public events;
+    // Check if a user has already rated event
+    mapping (uint256 => mapping (address => bool)) public hasRated;
+
+    struct Event {
+        string name;
+        string host;
+        string venue;
+        string imageUrl;
+        string lumaLink;
+        uint256 start;
+        uint256 end;
+        Stats stats;
+    }
+
+    struct Stats {
+        int8 overall;
+        int8 food;
+        int8 technical;
+        int8 networking;
+        int8 fun;
+        int8 swag;
+    }
+
+    function createEvent(
+        string memory _name,
+        string memory _host,
+        string memory _venue,
+        string memory _imageUrl,
+        string memory _lumaLink,
+        uint256 _start,
+        uint256 _end
+    ) public {
+        Stats memory initialStats = Stats(0, 0, 0, 0, 0, 0);
+        
+        events.push(Event({
+            name: _name,
+            host: _host,
+            venue: _venue,
+            imageUrl: _imageUrl,
+            lumaLink: _lumaLink,
+            start: _start,
+            end: _end,
+            stats: initialStats
+        }));
+    }
+
+    function createEvents(
+        string[] memory _names,
+        string[] memory _hosts,
+        string[] memory _venues,
+        string[] memory _imageUrls,
+        string[] memory _lumaLinks,
+        uint256[] memory _starts,
+        uint256[] memory _ends
+    ) public {
+        require(
+            _names.length == _venues.length &&
+            _venues.length == _imageUrls.length &&
+            _imageUrls.length == _lumaLinks.length &&
+            _lumaLinks.length == _starts.length &&
+            _starts.length == _ends.length &&
+            _hosts.length == _names.length,
+            "Array lengths must match"
+        );
+
+        for (uint i = 0; i < _names.length; i++) {
+            createEvent(
+                _names[i],
+                _hosts[i],
+                _venues[i],
+                _imageUrls[i],
+                _lumaLinks[i],
+                _starts[i],
+                _ends[i]
+            );
+        }
+    }
+
+    // Vote types: 1 for upvote, 2 for downvote, 0 for no vote
+    function rateEvent(
+        uint256 eventId,
+        uint8 overallVote,
+        uint8 foodVote,
+        uint8 technicalVote,
+        uint8 networkingVote,
+        uint8 funVote,
+        uint8 swagVote
+    ) public {
+        require(eventId < events.length, "Event does not exist");
+        require(overallVote == 1 || overallVote == 2, "Overall vote is required");
+        require(!hasRated[eventId][msg.sender], "User has already rated this event");
+
+        // Verify user is checked into event in Luma (eg. they actually attended)
+        verifyAttendance(eventId, msg.sender);
+
+        Event storage event_ = events[eventId];
+        
+        // Update overall rating (required)
+        if (overallVote == 1) {
+            event_.stats.overall += 1;
+        } else {
+            event_.stats.overall -= 1;
+        }
+        
+        // Update optional ratings
+        if (foodVote != 0) {
+            event_.stats.food += (foodVote == 1) ? int8(1) : int8(-1);
+        }
+        if (technicalVote != 0) {
+            event_.stats.technical += (technicalVote == 1) ? int8(1) : int8(-1);
+        }
+        if (networkingVote != 0) {
+            event_.stats.networking += (networkingVote == 1) ? int8(1) : int8(-1);
+        }
+        if (funVote != 0) {
+            event_.stats.fun += (funVote == 1) ? int8(1) : int8(-1);
+        }
+        if (swagVote != 0) {
+            event_.stats.swag += (swagVote == 1) ? int8(1) : int8(-1);
+        }
+
+        hasRated[eventId][msg.sender] = true;
+    }
+
+    // Verify user is checked into event in Luma (eg. they actually attended). Placeholder. Assuming we had access to the Luma API which is $70/mo :(
+    function verifyAttendance(uint256 eventId, address user) public returns (bool) {
+        return true;
+    }   
+}
